f544adbfde0167790ba5801bc6129baa227eb133
brandon11782 <bneese@live.com>
2019-03-06 23:54:17 -0500

    Inital commit

---
+++ app/src/main/java/com/csci490bn/calculator_bneese/MainActivity.java
@@ -0,0 +1,91 @@
+package com.csci490bn.calculator_bneese;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView calcText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calcText = findViewById(R.id.calcText);
+    }
+
+    public void Clear(View v){
+
+    }
+
+    public void ClearEverything(View v){
+
+    }
+
+    public void Add(View v){
+
+    }
+
+    public void Subtract(View v){
+
+    }
+
+    public void Multiply(View v){
+
+    }
+
+    public void Divide(View v){
+
+    }
+
+    public void Equals(View v){
+
+    }
+
+    public void DecimalInput(View v){
+
+    }
+
+    public void OneInput(View v){
+
+    }
+
+    public void TwoInput(View v){
+
+    }
+
+    public void ThreeInput(View v){
+
+    }
+
+    public void FourInput(View v){
+
+    }
+
+    public void FiveInput(View v){
+
+    }
+
+    public void SixInput(View v){
+
+    }
+
+    public void SevenInput(View v){
+
+    }
+
+    public void EightInput(View v){
+
+    }
+
+    public void NineInput(View v){
+
+    }
+
+    public void ZeroInput(View v){
+
+    }
+}
