ee3e3ecc2994ea3ba58b6305363fe413d482701e
brandon11782 <bneese@live.com>
2019-03-13 14:34:38 -0400

    button functionality
    crashes on startup, can't figure out why yet

--- app/src/main/java/com/csci490bn/calculator_bneese/MainActivity.java
+++ app/src/main/java/com/csci490bn/calculator_bneese/MainActivity.java
@@ -8,7 +8,9 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView calcText;
-    float answer;
+    double answer;
+    double heldNum;
+    boolean adding, subtracting, multiplying, dividing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,87 +20,120 @@ public class MainActivity extends AppCompatActivity {
         calcText = findViewById(R.id.calcText);
     }
 
-    public void Clear(View v){
+    public void Clear(View v) {
         calcText.setText(calcText.getText().subSequence(0, calcText.getText().length() - 1));
     }
 
-    public void ClearEverything(View v){
+    public void ClearEverything(View v) {
         calcText.setText("");
     }
 
-    public void Add(View v){
-        if (calcText.getText().length() != 0)
-            calcText.append(getResources().getString(R.string.add));
+    public void Add(View v) {
+        if (calcText.getText().length() != 0) {
+            heldNum = Double.parseDouble(calcText.getText().toString());
+            ClearEverything(v);
+            adding = true;
+            multiplying = false;
+            subtracting = false;
+            dividing = false;
+        }
     }
 
-    public void Subtract(View v){
-        if (calcText.getText().length() != 0)
-            calcText.append(getResources().getString(R.string.subtract));
+
+    public void Subtract(View v) {
+        if (calcText.getText().length() != 0) {
+            heldNum = Double.parseDouble(calcText.getText().toString());
+            ClearEverything(v);
+            adding = false;
+            multiplying = false;
+            subtracting = true;
+            dividing = false;
+        }
     }
 
-    public void Multiply(View v){
-        if (calcText.getText().length() != 0)
-            calcText.append(getResources().getString(R.string.multiply));
+    public void Multiply(View v) {
+        if (calcText.getText().length() != 0) {
+            heldNum = Double.parseDouble(calcText.getText().toString());
+            ClearEverything(v);
+            adding = false;
+            multiplying = true;
+            subtracting = false;
+            dividing = false;
+        }
     }
 
-    public void Divide(View v){
-        if (calcText.getText().length() != 0)
-            calcText.append(getResources().getString(R.string.divide);
+    public void Divide(View v) {
+        if (calcText.getText().length() != 0) {
+            heldNum = Double.parseDouble(calcText.getText().toString());
+            ClearEverything(v);
+            adding = false;
+            multiplying = false;
+            subtracting = false;
+            dividing = true;
+        }
     }
 
-    public void Equals(View v){
-        char [] equation = calcText.getText().toString().toCharArray();
-        answer = Equals(equation);
+    public void Equals(View v) {
+        double currentNum = Double.parseDouble(calcText.getText().toString());
+        if (adding) {
+            answer = currentNum + heldNum;
+        }
+        if (subtracting) {
+            answer = heldNum - currentNum;
+        }
+        if (multiplying) {
+            answer = heldNum * currentNum;
+        }
+        if (dividing) {
+            answer = heldNum / currentNum;
+        }
 
         calcText.setText(String.valueOf(answer));
     }
 
-    public void DecimalInput(View v){
+    public void DecimalInput(View v) {
         calcText.append(getResources().getString(R.string.point));
     }
 
-    public void OneInput(View v){
+    public void OneInput(View v) {
         calcText.append(getResources().getString(R.string.one));
     }
 
-    public void TwoInput(View v){
+    public void TwoInput(View v) {
         calcText.append(getResources().getString(R.string.two));
     }
 
-    public void ThreeInput(View v){
+    public void ThreeInput(View v) {
         calcText.append(getResources().getString(R.string.three));
     }
 
-    public void FourInput(View v){
+    public void FourInput(View v) {
         calcText.append(getResources().getString(R.string.four));
     }
 
-    public void FiveInput(View v){
+    public void FiveInput(View v) {
         calcText.append(getResources().getString(R.string.five));
     }
 
-    public void SixInput(View v){
+    public void SixInput(View v) {
         calcText.append(getResources().getString(R.string.six));
     }
 
-    public void SevenInput(View v){
+    public void SevenInput(View v) {
         calcText.append(getResources().getString(R.string.seven));
     }
 
-    public void EightInput(View v){
+    public void EightInput(View v) {
         calcText.append(getResources().getString(R.string.eight));
     }
 
-    public void NineInput(View v){
+    public void NineInput(View v) {
         calcText.append(getResources().getString(R.string.nine));
     }
 
-    public void ZeroInput(View v){
+    public void ZeroInput(View v) {
         calcText.append(getResources().getString(R.string.zero));
     }
+}
 
-
-    public float Equals(char[] equation) {
-
-    }
 
